025bd07f64c9428d4f340804bc5476f77116edc1
hummingengineer <hummingengineer@gmail.com>
2020-01-30 21:43:13 +0900

    Add new object to todos array

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useCallback } from 'react'
 import TodoTemplate from './components/TodoTemplate'
 import TodoInsert from './components/TodoInsert'
 import TodoList from './components/TodoList'
@@ -10,9 +10,18 @@ function App() {
     { id: 3, text: '일정 관리 앱 만들어 보기', checked: false }
   ])
 
+  // 고윳값으로 사용될 id
+  // ref를 사용하여 변수 담기
+  const nextId = useRef(4)  // useState가 아닌 useRef를 사용하여 컴포넌트에서 사용할 변수를 만드는 이유는 id 값은 렌더링되는 정보가 아니기 때문이다. 단순히 새로운 항목을 만들 때 참조되는 값일 뿐이다.
+  const onInsert = useCallback(text => {                      // todos 배열에 새 객체를 추가하는 onInsert 함수. props로 전달해야 할 함수를 만들 때는 useCallback을 사용하여 함수를 감싸는 것을 습관화 하자.
+    const todo = { id: nextId.current, text, checked: false }
+    setTodos(todos.concat(todo))
+    nextId.current += 1  // nextId 1씩 더하기
+  }, [todos])  // todos가 바뀌었을 때만 함수 생성
+
   return (
     <TodoTemplate>
-      <TodoInsert/>
+      <TodoInsert onInsert={onInsert}/>
       <TodoList todos={todos}/>  {/* todos 배열이 TodoList에 props로 전달된다. TodoList에서 이 값을 받아 TodoItem으로 변환하여 렌더링 된다. */}
     </TodoTemplate>
   )
--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -7,9 +7,9 @@ import './TodoInsert.scss'
 
 function TodoInsert () {
   const [value, setValue] = useState('')
-  const onChange = useCallback(e => {  // 인풋에 넣어 줄 onChange 함수
+  const onChange = useCallback(e => {  // 인풋에 넣어 줄 onChange 함수. 컴포넌트가 처음 렌더링 될 때만 함수 생성.
     setValue(e.target.value)           // 컴포넌트가 리렌더링될 때마다 함수를 새로 만드는 것이 아니라, 한 번 함수를 만들고 재사용할 수 있도록 useCallback Hook 사용
-  }, [])
+  }, [])  // 비어 있는 배열을 넣게 되면 컴포넌트가 렌더링될 때 단 한번만 함수가 생성
 
   return (
     <form className="TodoInsert">
